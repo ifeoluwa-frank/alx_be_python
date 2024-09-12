c2cb8481c01bf6941d606cfb4cbf5ced92818cff
Ifeoluwa Frank <ifeoluwa.frank@yahoo.com>
2024-09-12 12:18:59 +0100

    arithmetic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+# define and assing values to variables
+number1 = 10
+number2 = 5
+
+# Perform arithmetic operations
+sum = number1 + number2
+difference = number1 - number2
+multiplication = number1 * number2
+
+# Print output
+print("Addition of " + str(number1) + " and " + str(number2) + " is " + str(sum))
+print("Subtraction of " + str(number1) + " and " + str(number2) + " is " + str(difference))
+print("Multiplication of " + str(number1) + " and " + str(number2) + " is " + str(multiplication))
