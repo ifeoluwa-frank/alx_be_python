0cf628fefba38faaac12040fff165a5e835f48ec
Ifeoluwa Frank <ifeoluwa.frank@yahoo.com>
2024-09-27 12:25:31 +0100

    More fix

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -7,10 +7,10 @@ def perform_operation(num1, num2, operation):
         case "multiply":
             result = num1 * num2
         case "divide":
-            if(num2 > 0):
-                result = num1 / num2
-            elif(num2 == 0):
+            if(num2 == 0):
                 result = "Cannot divide by zero"
+            elif(num2 > 0):
+                result = num1 / num2
         case _:
             result = "Not a valid operator"
     return result
