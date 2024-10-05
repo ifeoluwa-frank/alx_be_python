717644240244e7111bbf46a0ad6053168b656d30
Ifeoluwa Frank <ifeoluwa.frank@yahoo.com>
2024-10-05 15:14:11 +0100

    Create a Simple Bank Account Class

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,18 @@
+class BankAccount: 
+    def __init__(self, account_balance, initial_balance = 0):
+        self.account_balance = account_balance
+        self.initial_balance = initial_balance
+    
+    def deposit(self, amount):
+        self.account_balance += amount
+    
+    def withdraw(self, amount):
+        if amount <= self.account_balance:
+            self.account_balance -= amount
+            return 1
+        else:
+            return 0
+        
+    def display_balance(self):
+        balance = f"Current balance: {self.account_balance}"
+        print(balance)
