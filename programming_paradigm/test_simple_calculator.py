622f70067f6a8c8254101659cd10d18c28ff0f0d
Ifeoluwa Frank <ifeoluwa.frank@yahoo.com>
2024-10-05 23:15:38 +0100

    fixes

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -7,31 +7,26 @@ class TestSimpleCalculator(unittest.TestCase):
         self.calc = SimpleCalculator()
 
     def test_addition_positive(self):
-        result = SimpleCalculator.add(self, 5, 3)
-        self.assertEqual(result, 8)
+        self.assertEqual(self.calc.add(10, 5), 15)
 
     def test_add_negative(self):
-        result = SimpleCalculator.add(self, -2, 7)
-        self.assertEqual(result, 5)
+        self.assertEqual(self.calc.add(-10, 5), -5)
 
     def test_sub_positive(self):
-        result = SimpleCalculator.subtract(self, 10, 5)
-        self.assertEqual(result, 5)
+        self.assertEqual(self.calc.subtract(10, 5), 5)
 
     def test_sub_negative(self):
-        result = SimpleCalculator.subtract(self, -5, 5)
-        self.assertEqual(result, -10)
+        self.assertEqual(self.calc.subtract(-10, 5), -15)
 
     def test_multiply(self):
-        result = SimpleCalculator.multiply(self, 5, 3)
-        self.assertEqual(result, 15)
+        self.assertEqual(self.calc.multiply(5, 3), 15)
 
     def test_divide(self):
         self.assertEqual(self.calc.divide(10, 5), 2)
 
     def test_divide_by_zero(self):
         with self.assertRaises(ZeroDivisionError):
-            SimpleCalculator.divide(10, 0)
+            SimpleCalculator.divide(self, 10, 0)
 
 if __name__ == "__main__":
   unittest.main()
