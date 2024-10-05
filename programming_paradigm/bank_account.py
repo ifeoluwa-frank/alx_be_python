884c50028b11119cfcb66cd3d7addaeefdb25b77
Ifeoluwa Frank <ifeoluwa.frank@yahoo.com>
2024-10-05 15:22:19 +0100

    fix

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,14 +1,14 @@
 class BankAccount: 
     def __init__(self, account_balance, initial_balance = 0):
-        self.account_balance = account_balance
+        self.account_balance = float(account_balance)
         self.initial_balance = initial_balance
     
     def deposit(self, amount):
-        self.account_balance += amount
+        self.account_balance += float(amount)
     
     def withdraw(self, amount):
         if amount <= self.account_balance:
-            self.account_balance -= amount
+            self.account_balance -= float(amount)
             return 1
         else:
             return 0
